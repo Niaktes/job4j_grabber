0e55b21783179d7247995912107f7fba9ff39acc
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2022-09-06 14:06:44 +0300

    2.3.7.2.1 HabrCareerDateTimeParser parse() method simplified.

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,18 +1,13 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.time.ZonedDateTime;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        String[] dateTime = parse.split("\\D");
-        Integer[] numVal = Arrays.stream(dateTime)
-                .limit(6)
-                .map(Integer :: parseInt)
-                .toArray(Integer[] :: new);
-        return LocalDateTime.of(numVal[0], numVal[1], numVal[2], numVal[3], numVal[4], numVal[5]);
+        return ZonedDateTime.parse(parse).toLocalDateTime();
     }
 
 }
