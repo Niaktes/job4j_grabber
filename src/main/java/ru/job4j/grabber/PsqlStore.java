16923b1a7b6762f768bafa9daa7bfec864356b86
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2022-09-12 16:25:03 +0300

    2.3.7.5 PsqlStore constructor fixed and main() method added.

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -1,15 +1,29 @@
 package ru.job4j.grabber;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class PsqlStore implements Store, AutoCloseable {
 
     private Connection cn;
 
-    public PsqlStore(Connection connection) {
-        this.cn = connection;
+    public PsqlStore(Properties cfg) throws SQLException {
+        try {
+            Class.forName(cfg.getProperty("store.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        this.cn = DriverManager.getConnection(
+                cfg.getProperty("store.url"),
+                cfg.getProperty("store.login"),
+                cfg.getProperty("store.password")
+        );
     }
 
 
@@ -80,6 +94,29 @@ public class PsqlStore implements Store, AutoCloseable {
                 resultSet.getString("text"),
                 resultSet.getTimestamp("created").toLocalDateTime()
         );
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Post first = new Post("one", "http://1", "first", LocalDateTime.parse("2007-01-01T10:11:11"));
+        Post second = new Post("two", "http://2", "second", LocalDateTime.parse("2007-02-02T20:22:22"));
+        Post third = new Post("three", "http://3", "third", LocalDateTime.parse("2007-03-03T21:33:33"));
+        Properties cfg = new Properties();
+        try (InputStream in = Grab.class.getClassLoader().getResourceAsStream("habrCareerGrabber.properties")) {
+            cfg.load(in);
+        } catch (IOException e) {
+            throw new FileNotFoundException("Cannot find property file. Please, check file name.");
+        }
+        Store store = new PsqlStore(cfg);
+        store.save(first);
+        store.save(second);
+        store.save(third);
+        store.save(first);
+        store.save(first);
+        System.out.println(store.findById(2));
+        List<Post> posts = store.getAll();
+        System.out.println(posts.size());
+        posts.forEach(System.out::println);
     }
 
 }
